cfa86115747407d07108593e5a1a429339473e12
risktoollib <coteph@mac.com>
2014-05-19 09:55:01 -0600

    garch fix + added lattice in depends

--- R/garch.R
+++ R/garch.R
@@ -2,7 +2,7 @@
 #' @description Returns a xts object representing the average of multiple averages.
 #' @param x Univariate or multivariate xts price series.
 #' @param ret "rel" for relative returns, "abs" for absolute returns or "flatprice" if no transformation of x is require.
-#' @return xts series of annualised Garch(1,1) volatilities. 
+#' @return xts series of annualised Garch(1,1) volatilities if using relative returns.
 #' @export garch
 #' @author Philippe Cote <coteph@@mac.com>, Nima Safaian <nima.safaian@@gmail.com>
 #' @examples 
@@ -34,15 +34,10 @@ garch <- function(x=data,ret="rel",roll=TRUE,cmdty="") {
   x<-res[,-1]
     
   if(ret=="rel") {
-  # Annualized Volatilty
-  if (periodicity(x)$scale=="daily") {x$garchvol <- x$garchvol*sqrt(250)}
-  if (periodicity(x)$scale=="weekly") {x$garchvol <- x$garchvol*sqrt(52)}
-  if (periodicity(x)$scale=="monthly") {x$garchvol <- x$garchvol*sqrt(12)}
-  } else {
-  if (periodicity(x)$scale=="daily") {x$garchvol <- x$garchvol}
-  if (periodicity(x)$scale=="weekly") {x$garchvol <- x$garchvol}          
-  if (periodicity(x)$scale=="monthly") {x$garchvol <- x$garchvol}          
-  }
+    if (periodicity(x)$scale=="daily") {x <- x*sqrt(250)}
+    if (periodicity(x)$scale=="weekly") {x <- x*sqrt(52)}
+    if (periodicity(x)$scale=="monthly") {x <- x*sqrt(12)}
+  } 
   colnames(x) <- colnames(varnames)
   return(x)
 }
